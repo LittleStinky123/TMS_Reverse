6e783ff00588a3d3da877ed45ada9249f9125aea
LittleStinky123 <arsch.kackewurst123@gmail.com>
2020-01-19 21:26:03 +0100

    Add initial programm, still wrong offsets

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+
+public class Example
+{
+    public static void Main()
+    {
+        int[,] array2D = new int[,] { 
+            { 0x00000, 0x0008F, 1}, {0x00090, 0x000CF, 2}, {0x000D0, 0x020AF, 0}, {0x020B0, 0x020FF, 1}, {0x02100, 0x34EFF, 0},{ 0x34F00, 0x3541F, 2},{ 0x35420, 0x4191F, 1},
+            { 0x41920, 0x44CDF, 2},{ 0x44CE0, 0x450DF, 1}, {0x450E0, 0x4519F, 2}, {0x451A0, 0x451BF, 0}, {0x451C0, 0x4529F, 2}, {0x452A0, 0x452BF, 0}, {0x452C0, 0x4539F, 2},
+            { 0x453A0, 0x453BF, 0}, {0x453C0, 0x4549F, 2}, {0x454A0, 0x454BF, 0}, {0x454C0, 0x4559F, 2}, {0x455A0, 0x455BF, 0}, {0x455C0, 0x4569F, 2}, {0x456A0, 0x456BF, 0},
+            { 0x456C0, 0x4579F, 2}, {0x457A0, 0x457BF, 0}, {0x457C0, 0x4589F, 2}, {0x458A0, 0x459BF, 0}, {0x459C0, 0x45A9F, 2}, {0x45AA0, 0x45ABF, 0}, {0x45AC0, 0x45B9F, 2},
+            { 0x45BA0, 0x45BBF, 0}, {0x45BC0, 0x45C9F, 2}, {0x45CA0, 0x45CBF, 0}, {0x45CC0, 0x45D9F, 2},{ 0x45DA0, 0x45DBF, 0}, {0x45DC0, 0x45E9F, 2}, {0x45EA0, 0x45EBF, 0},
+            { 0x45EC0, 0x45F9F, 2}, {0x45FA0, 0x45FBF, 0}, {0x45FC0, 0x4609F, 2}, {0x460A0, 0x460BF, 0}, {0x460C0, 0x4619F, 2}, {0x461A0, 0x461BF, 0},{ 0x461C0, 0x4A06F, 2},
+            { 0x4A070, 0x51DAF, 1}, {0x51DB0, 0x520BF, 2}, {0x520C0, 0x535BF, 0}};
+        byte[] bytesOrig = System.IO.File.ReadAllBytes("L:/save_1_switch.bin");
+        // Offset after which endian is swapped?
+        //int pos = 0x535BF;
+        FileStream file = File.Create("L:/save_1_switch_converted.bin");
+        byte[] twobyteswap = new byte[2];
+        byte[] fourbyteswap = new byte[4];
+        byte[] bytescopy = new byte[2];
+        int start, end = 0;
+        int sum = 0;
+        for (int i = 0; i < array2D.GetLength(0); i++)
+        {
+            sum += (array2D[i, 1]- array2D[i, 0]);
+            //Console.WriteLine(sum);
+            //Console.WriteLine(i);
+            Console.WriteLine(array2D[i,0]);
+            Console.WriteLine(array2D[i, 1]);
+            //Console.WriteLine(array2D[i, 2]);
+            switch (array2D[i, 2])
+            {
+                //Copy bytes
+                case 0:
+                    Console.WriteLine(i);
+                    Console.WriteLine((array2D[i, 1]- array2D[i, 1])%2);
+                    start = array2D[i, 0];
+                    end = array2D[i, 1];
+                    while (start < end)
+                    {
+                        Array.Copy(bytesOrig, start, bytescopy, 0, 2);
+                        file.Write(bytescopy, 0, bytescopy.Length);
+                        start += 2;
+                    }
+                    break;
+                // Swap every 2 bytes
+                case 1:
+                    Console.WriteLine(i);
+                    Console.WriteLine((array2D[i, 1] - array2D[i, 1]) % 2);
+                    start = array2D[i, 0];
+                    end = array2D[i, 1];
+                    while (start < end)
+                    {
+                        Array.Copy(bytesOrig, start, twobyteswap, 0, 2);
+                        Array.Reverse(twobyteswap);
+                        file.Write(twobyteswap, 0, twobyteswap.Length);
+                        start += 2;
+                    }
+                    break;
+                // Swap every 4 bytes
+                case 2:
+                    Console.WriteLine(i);
+                    Console.WriteLine((array2D[i, 1] - array2D[i, 1]) % 4);
+                    start = array2D[i, 0];
+                    end = array2D[i, 1];
+                    while (start < end)
+                    {
+                        Array.Copy(bytesOrig, start, fourbyteswap, 0, 4);
+                        Array.Reverse(fourbyteswap);
+                        file.Write(fourbyteswap, 0, fourbyteswap.Length);
+                        start += 4;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        file.Close();
+    }
+    }
+// The example displays the following output on a little-endian system:
+//       4E-61-BC-00
+//       00-BC-61-4E
+//       00-BC-61-4E
+//       4E-61-BC-00
+//       Original value: 12345678
+//       Returned value: 12345678
